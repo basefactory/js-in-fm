3a90f4578ac3a0a1c08e507ea82992810ed509c2
Rutger Meeuwis <rutger@basefactory.be>
2023-04-02 08:27:17 +0200

    teest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-//Here we're importing items we'll need. You can add other imports here.
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+import View from 'ol/View';
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+const layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+  new TileLayer({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new TileWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: {'LAYERS': 'topp:states', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0,
+    }),
+  }),
+];
+const map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center: [-10997148, 4569099],
+    zoom: 4,
+  }),
+});
